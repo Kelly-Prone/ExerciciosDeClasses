5a04725ee428defb0961a24c18771b50fc9bfbfd
Kelly Prone <72274839+Kelly-Prone@users.noreply.github.com>
2025-01-06 15:48:53 -0300

    Códigos C#

---
+++ Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_classes_03
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado()
+        {
+            return NotaFinal() >= 60.0;
+        }
+
+        public double Pontos()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            return 60.0 - NotaFinal();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using exercicio_classes_03;
+
+/* Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+(primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
+ano. Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam
+para o aluno obter o mínimo para ser aprovado (que é 60 pontos). 
+Você deve criar uma classe Aluno para resolver este problema. */
+
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno x = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            x.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            x.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"\nNOTA FINAL = " + x.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (x.Aprovado())
+            {
+                Console.WriteLine("** APROVADO **");
+            }
+            else
+            {
+                Console.WriteLine("** REPROVADO **");
+                Console.WriteLine($"FALTARAM " + x.Pontos().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
